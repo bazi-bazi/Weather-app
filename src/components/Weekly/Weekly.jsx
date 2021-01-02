dce21043f9b0618300f7001b3df38afe57c26073
bazi <vazha.davituliani@gmail.com>
2021-01-02 20:24:21 +0400

    commit

--- src/components/Weekly/Weekly.jsx
+++ src/components/Weekly/Weekly.jsx
@@ -1,9 +1,41 @@
 import React from "react"
-
+import "./weekly.css"
+import sun from "../../img/sun.svg"
 
 const Weekly =() => {
   return (
-    <h2>Weekly Weather</h2>
+    <div className="weekly">
+      <div className="weekly__content">
+        <p className="weekly__title">tue</p>
+        <img src={sun} alt="tuesday" className="weekly__icon" />
+        <p className="weekly__number">22&#176;</p>
+      </div>
+      <div className="weekly__content">
+        <p className="weekly__title">wed</p>
+        <img src={sun} alt="tuesday" className="weekly__icon" />
+        <p className="weekly__number">22&#176;</p>
+      </div>
+      <div className="weekly__content">
+        <p className="weekly__title">thu</p>
+        <img src={sun} alt="tuesday" className="weekly__icon" />
+        <p className="weekly__number">22&#176;</p>
+      </div>
+      <div className="weekly__content">
+        <p className="weekly__title">fir</p>
+        <img src={sun} alt="tuesday" className="weekly__icon" />
+        <p className="weekly__number">22&#176;</p>
+      </div>
+      <div className="weekly__content">
+        <p className="weekly__title">sat</p>
+        <img src={sun} alt="tuesday" className="weekly__icon" />
+        <p className="weekly__number">22&#176;</p>
+      </div>
+      <div className="weekly__content">
+        <p className="weekly__title">sun</p>
+        <img src={sun} alt="tuesday" className="weekly__icon" />
+        <p className="weekly__number">22&#176;</p>
+      </div>
+    </div>
   )
 }
 
